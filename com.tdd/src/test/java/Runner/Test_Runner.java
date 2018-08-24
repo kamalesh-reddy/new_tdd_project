e481fb42f2cb0ea2aa0d055ae5bdd11c828b894c
IBM <IBM@KAMAL-REDDY>
2018-08-24 22:32:38 +0530

    sent

---
+++ com.tdd/src/test/java/Runner/Test_Runner.java
@@ -0,0 +1,30 @@
+package Runner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test_Runner {
+WebDriver driver;
+@Test
+public void test() 
+{
+	System.out.println(driver.getTitle());
+}
+@BeforeTest
+public void beforeTest()
+{
+	System.setProperty("webdriver.gecko.driver", "/gecko/geckodriver");
+	//System.setProperty("webdriver.gecko.driver", "E:\\SHARED\\geckodriver\\geckodriver.exe");
+	System.out.println("helllooo");
+	driver= new FirefoxDriver();
+	driver.get("http://www.facebook.com");
+
+}
+@AfterTest
+public void afterTest(){
+	System.out.println("Running1");
+}
+}
